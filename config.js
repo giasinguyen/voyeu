37b78aeb2170b06c97a83a633d3203e5c0644732
giasinguyen <giasinguyentran@gmail.com>
2022-10-15 18:06:13 +0700

    xui

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Bé Xúi';
+const giftUrl = 'https://www.facebook.com/NYN.inhy';
+const eventName = '20/10 Bé Xúi';
+const titleCard = 'Tặng bé xúi';
+const contentCard = 'Chúc vợ yêu 20/10 vui vẻ, chúc bé xúi luôn mạnh giỏi, khoẻ mạnh và bình an. Gấu yêu Bé Xúiiii';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
